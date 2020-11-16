5340e0bd1711e68c79757e53a337b13a40ba73a2
Simon Josefsson <simon@josefsson.org>
2020-11-16 08:17:11 +0100

    Allow easier re-use out-of-package.

--- oathtool/tests/tst_oathtool.sh
+++ oathtool/tests/tst_oathtool.sh
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-OATHTOOL=../oathtool
+OATHTOOL=${OATHTOOL:-../oathtool}
 
 ME_=`expr "./$0" : '.*/\(.*\)$'`
 
