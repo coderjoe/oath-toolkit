ff7f814c5f4fce00917cf60bafea0e9591fab3ed
Nick Gaya <nicholasgaya+github@gmail.com>
2021-06-17 10:21:42 -0700

    pam_oath: Provide fallback pam_modutil_getpwnam implementation
    Closes #26

--- pam_oath/pam_oath.c
+++ pam_oath/pam_oath.c
@@ -21,6 +21,7 @@
 #include <config.h>
 
 #include "oath.h"
+#include "pam_modutil.h"
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -42,9 +43,6 @@
 
 #ifdef HAVE_SECURITY_PAM_APPL_H
 #include <security/pam_appl.h>
-#endif
-#ifdef HAVE_SECURITY_PAM_MODUTIL_H
-#include <security/pam_modutil.h>
 #endif
 #ifdef HAVE_SECURITY_PAM_MODULES_H
 #include <security/pam_modules.h>
